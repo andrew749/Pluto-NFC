ec5ae4ef84ab0f308809cf0c543c18a09f3d646e
Andrew Codispoti <andrewcod749@gmail.com>
2015-09-07 12:43:44 -0400

    Gets the serial numbers and opens the  endpoint on pluto.

--- app/src/main/java/me/andrewcodispoti/pluto_nfc/SendDataActivity.java
+++ app/src/main/java/me/andrewcodispoti/pluto_nfc/SendDataActivity.java
@@ -24,108 +24,34 @@ import java.util.Arrays;
 public class SendDataActivity extends Activity {
     String TAG = "PLUTO";
     public static final String MIME_TEXT_PLAIN = "text/plain";
-    public static final String PLUTO_PATH = "127.0.0.1:3000/login";
+    public static final String PLUTO_PATH = "192.168.0.1:3000/login";
     @Override
     protected void onCreate(Bundle savedInstanceState) {/**/
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         String action = intent.getAction();
         Toast.makeText(this,"Tag", Toast.LENGTH_LONG);
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
-
-            String type = intent.getType();
-            if (MIME_TEXT_PLAIN.equals(type)) {
-
-                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-                new NdefReaderTask().execute(tag);
-
-            } else {
-                Log.d(TAG, "Wrong mime type: " + type);
-            }
-        } else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
-
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
             // In case we would still use the Tech Discovered Intent
             Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            String[] techList = tag.getTechList();
-            String searchedTech = Ndef.class.getName();
-
-            for (String tech : techList) {
-                if (searchedTech.equals(tech)) {
-                    new NdefReaderTask().execute(tag);
-                    break;
+            byte[] tagId  = tag.getId();
+            String hexdump = new String();
+            for (int i = 0; i < tagId.length; i++) {
+                String x = Integer.toHexString(((int) tagId[i] & 0xff));
+                if (x.length() == 1) {
+                    x = '0' + x;
                 }
+                hexdump += x + ' ';
             }
+            new LoginTask().execute(hexdump.replace(" ", ":"));
         }
     }
-
-    private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
-
-        @Override
-        protected String doInBackground(Tag... params) {
-            Tag tag = params[0];
-
-            Ndef ndef = Ndef.get(tag);
-            if (ndef == null) {
-                // NDEF is not supported by this Tag.
-                return null;
-            }
-
-            NdefMessage ndefMessage = ndef.getCachedNdefMessage();
-
-            NdefRecord[] records = ndefMessage.getRecords();
-            for (NdefRecord ndefRecord : records) {
-                if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
-                    try {
-                        return readText(ndefRecord);
-                    } catch (UnsupportedEncodingException e) {
-                        Log.e(TAG, "Unsupported Encoding", e);
-                    }
-                }
-            }
-
-            return null;
-        }
-
-        private String readText(NdefRecord record) throws UnsupportedEncodingException {
-        /*
-         * See NFC forum specification for "Text Record Type Definition" at 3.2.1
-         *
-         * http://www.nfc-forum.org/specs/
-         *
-         * bit_7 defines encoding
-         * bit_6 reserved for future use, must be 0
-         * bit_5..0 length of IANA language code
-         */
-
-            byte[] payload = record.getPayload();
-
-            // Get the Text Encoding
-            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
-
-            // Get the Language Code
-            int languageCodeLength = payload[0] & 0063;
-
-            // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
-            // e.g. "en"
-
-            // Get the Text
-            return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
-        }
+    private class LoginTask extends AsyncTask<String, Void, Void>{
 
         @Override
-        protected void onPostExecute(String result) {
-            if (result != null) {
-//                mTextView.setText("Read content: " + result);
-                    new LoginTask().execute();
-            }
-        }
-    }
-    private class LoginTask extends AsyncTask<Void, Void, Void>{
-
-        @Override
-        protected Void doInBackground(Void... params) {
+        protected Void doInBackground(String... params) {
             try {
-                URL url = new URL(PLUTO_PATH);
+                URL url = new URL(PLUTO_PATH + params[0]);
                 HttpURLConnection conn = (HttpURLConnection)url.openConnection();
                 conn.connect();
             } catch (IOException e) {
